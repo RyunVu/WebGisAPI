a311b3ec3ffcc00234d16e4b5ad44e6859e6a982
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-03-28 10:52:49 +0700

    create model and contracts for page list and validation

---
+++ src/WebGis.Core/Contracts/IPagingParams.cs
@@ -0,0 +1,10 @@
+﻿namespace WebGis.Core.Contracts
+{
+	public interface IPagingParams
+	{
+		int? PageSize { get; set; }
+		int? PageNumber { get; set; }
+		string SortColumn { get; set; }
+		string SortOrder { get; set; }
+	}
+}
