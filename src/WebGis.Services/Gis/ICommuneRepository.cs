52e9d1bcd0917ae24b7435f109a2beee72e2c53a
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-04-02 14:13:32 +0700

    create repository for commune

---
+++ src/WebGis.Services/Gis/ICommuneRepository.cs
@@ -0,0 +1,43 @@
+﻿using WebGis.Core.Contracts;
+using WebGis.Core.Entities;
+using WebGis.Core.Queries;
+
+namespace WebGis.Services.Gis
+{
+	public interface ICommuneRepository
+	{
+		Task<IPagedList<T>> GetPagedCommuneAsync<T>(
+		CommuneQuery query,
+		IPagingParams pagingParams,
+		Func<IQueryable<Commune>, IQueryable<T>> mapper,
+		CancellationToken cancellationToken = default);
+
+		Task<IList<Commune>> GetCommunesAsync(
+			CancellationToken cancellationToken = default);
+		Task<Commune> GetCommuneByIdAsync(
+			Guid id,
+			bool includeDetail = false,
+			CancellationToken cancellationToken = default);
+
+		Task<Commune> GetCommuneBySlugAsync(
+			string slug,
+			CancellationToken cancellationToken = default);
+
+		Task<bool> IsCommuneIdExistedAsync(
+			Guid id,
+			CancellationToken cancellationToken = default);
+
+		Task<bool> IsCommuneSlugExistedAsync(
+			Guid id,
+			string slug,
+			CancellationToken cancellationToken = default);
+
+		Task<bool> AddOrUpdateCommuneAsync(
+			Commune commune,
+			CancellationToken cancellationToken = default);
+
+		Task<bool> DeleteCommuneByIdAsync(
+			Guid id,
+			CancellationToken cancellationToken = default);
+	}
+}
