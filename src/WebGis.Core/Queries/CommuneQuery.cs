3d3ffd30a8525cbb950e9eb45e2ff67f43d8a20f
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-04-02 13:41:05 +0700

    Create Query entity classes

---
+++ src/WebGis.Core/Queries/CommuneQuery.cs
@@ -0,0 +1,8 @@
+﻿namespace WebGis.Core.Queries
+{
+	public class CommuneQuery
+	{
+		public string Keyword { get; set; }
+
+	}
+}
---
+++ src/WebGis.Core/Queries/PlantOutput.cs
@@ -0,0 +1,8 @@
+﻿namespace WebGis.Core.Queries
+{
+	public class PlantOutput
+	{
+		public string Keyword { get; set; }
+
+	}
+}
