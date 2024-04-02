6c0ab80ff2037c41d61327c8065e4d36420c2364
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-04-02 13:43:07 +0700

    Create Validation class for district

---
+++ src/WebGis.WebAPI/Validations/DistrictValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using WebGis.WebAPI.Models;
+
+namespace WebGis.WebAPI.Validations
+{
+	public class DistrictValidator : AbstractValidator<DistrictEditModel>
+	{
+        public DistrictValidator()
+        {
+			RuleFor(c => c.Name)
+				.NotEmpty()
+				.WithMessage("Tên chủ đề không được để trống")
+				.MaximumLength(128)
+				.WithMessage("Tên chủ đề tối đa 128 ký tự");
+
+			RuleFor(c => c.Description)
+				.NotEmpty()
+				.WithMessage("Phần giới thiệu không được để trống");
+
+			RuleFor(c => c.UrlSlug)
+				.NotEmpty()
+				.WithMessage("UrlSlug không được để trống")
+				.MaximumLength(128)
+				.WithMessage("UrlSlug tối đa 128 ký tự");
+		}
+    }
+}
