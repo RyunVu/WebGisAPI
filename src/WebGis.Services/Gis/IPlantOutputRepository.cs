459fa3d9118bbab8e62d51b6d6f9d0e2bca50c86
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-04-02 14:35:37 +0700

    Create Repository for Plant output

---
+++ src/WebGis.Services/Gis/IPlantOutputRepository.cs
@@ -0,0 +1,43 @@
+﻿using WebGis.Core.Contracts;
+using WebGis.Core.Entities;
+using WebGis.Core.Queries;
+
+namespace WebGis.Services.Gis
+{
+	public interface IPlantOutputRepository
+	{
+		Task<IPagedList<T>> GetPagedPlantAsync<T>(
+		PlantQuery query,
+		IPagingParams pagingParams,
+		Func<IQueryable<Plant>, IQueryable<T>> mapper,
+		CancellationToken cancellationToken = default);
+
+		Task<IList<Plant>> GetPlantsAsync(
+			CancellationToken cancellationToken = default);
+		Task<Plant> GetPlantByIdAsync(
+			Guid id,
+			bool includeDetail = false,
+			CancellationToken cancellationToken = default);
+
+		Task<Plant> GetPlantBySlugAsync(
+			string slug,
+			CancellationToken cancellationToken = default);
+
+		Task<bool> IsPlantIdExistedAsync(
+			Guid id,
+			CancellationToken cancellationToken = default);
+
+		Task<bool> IsPlantSlugExistedAsync(
+			Guid id,
+			string slug,
+			CancellationToken cancellationToken = default);
+
+		Task<bool> AddOrUpdatePlantAsync(
+			Plant plant,
+			CancellationToken cancellationToken = default);
+
+		Task<bool> DeletePlantByIdAsync(
+			Guid id,
+			CancellationToken cancellationToken = default);
+	}
+}
