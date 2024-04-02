76cccb23d1a1554549c086138eab0c9678513142
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-04-02 13:42:32 +0700

    Create Commune Model

---
+++ src/WebGis.WebAPI/Models/CommuneDto.cs
@@ -0,0 +1,10 @@
+﻿namespace WebGis.WebAPI.Models
+{
+	public class CommuneDto
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+		public string UrlSlug { get; set; }
+		public float Area { get; set; }
+	}
+}
