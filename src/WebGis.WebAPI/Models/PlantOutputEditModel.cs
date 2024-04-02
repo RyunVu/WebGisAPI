a82d8583fb38da73dc9fba0c0f2a7e9cefe146c0
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-04-02 14:47:35 +0700

    Create models for plantoutput

---
+++ src/WebGis.WebAPI/Models/PlantOutputEditModel.cs
@@ -0,0 +1,18 @@
+﻿namespace WebGis.WebAPI.Models
+{
+	public class PlantOutputEditModel
+	{
+		public int Quantity { get; set; }
+		public string UrlSlug { get; set; }
+
+		private DateTime _time;
+		public DateTime Time
+		{
+			get => _time;
+			set => _time = DateTime.SpecifyKind(value, DateTimeKind.Utc);
+		}
+
+		public Guid CommuneId { get; set; }
+		public Guid PlantId { get; set; }
+	}
+}
---
+++ src/WebGis.WebAPI/Models/PlantOutputFilterModel.cs
@@ -0,0 +1,7 @@
+﻿namespace WebGis.WebAPI.Models
+{
+	public class PlantOutputFilterModel : PagingModel
+	{
+        public string Keyword { get; set; }
+    }
+}
