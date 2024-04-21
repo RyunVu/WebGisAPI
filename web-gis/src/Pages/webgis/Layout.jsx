8683b97a799443eee71e90d28a120c5f29293089
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-04-21 20:30:22 +0700

    Update Layout.jsx

--- web-gis/src/Pages/webgis/Layout.jsx
+++ web-gis/src/Pages/webgis/Layout.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
-import Navbar from '../../Components/webgis/Navbar';
 import MapView from '../../Components/webgis/MapView';
-// import Sidebar from '../../Components/webgis/Sidebar';
 import GlobalStyle from '../../Components/webgis/WebStyle';
 
-export default function Layout({ setPlantId, setYear, setMonth }) {
+export default function Layout() {
     return (
         <>
             <GlobalStyle />
-            <Navbar setPlantId={setPlantId} setYear={setYear} setMonth={setMonth} />
             <MapView />
-            {/* <Sidebar /> */}
         </>
     );
 }
