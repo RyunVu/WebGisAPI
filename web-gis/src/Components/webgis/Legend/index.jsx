394a4dab35a61348e5adfbc40b679696b4c9eab2
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-04-21 20:29:47 +0700

    Create Legend component to display ranking

---
+++ web-gis/src/Components/webgis/Legend/index.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './Legend.module.scss';
+
+export default function Legend({ colors }) {
+    if (!colors || colors.length === 0) return null;
+
+    return (
+        <div className={styles.legend}>
+            <div className={styles.legendBox}>
+                <h3 className={styles.legendTitle}>Xếp hạng</h3>
+                <ul className={styles.legendList}>
+                    {colors &&
+                        colors.length > 0 &&
+                        colors.map((color, index) => (
+                            <li key={index} className={styles.legendItem}>
+                                <span className={styles.colorBox} style={{ backgroundColor: color }}></span>
+                                <span>Xếp hạng {index + 1}</span>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
