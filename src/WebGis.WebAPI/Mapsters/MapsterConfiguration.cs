c1836d9c6fd45161ec464daef58854ac90e685c6
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-05-03 18:30:31 +0700

    Update MapsterConfiguration.cs

--- src/WebGis.WebAPI/Mapsters/MapsterConfiguration.cs
+++ src/WebGis.WebAPI/Mapsters/MapsterConfiguration.cs
@@ -12,6 +12,7 @@ namespace WebGis.WebAPI.Mapsters
 			config.NewConfig<District, DistrictDto>();
 			config.NewConfig<District, DistrictItem>();
 
+
 			config.NewConfig<Category, CategoryDto>()
 				.Map(dest => dest.PlantsCount,
 					src => src.Plants == null ? 0 : src.Plants.Count);
