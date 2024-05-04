d3233c036443ae08094738df5f0c8a1b9f6ae76c
Ryun KuroShi <125938790+RyunVu@users.noreply.github.com>
2024-05-04 11:03:05 +0700

    Update plants.jsx
    Create funcs to fetch API

--- web-gis/src/Services/plants.jsx
+++ web-gis/src/Services/plants.jsx
@@ -1,16 +1,88 @@
 import axios from 'axios';
 
-export async function getPlants() {
-    // console.log('API endpoint:', process.env.REACT_APP_API_ENDPOINT);
+export async function getPlantsByQueries(queries) {
     try {
-        const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/plant`);
+        const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/plant?${queries}`);
         if (data.isSuccess) {
             return data.result;
         } else {
             return null;
         }
     } catch (error) {
-        console.error('Error fetching communes:', error);
+        console.error('Có lỗi:', error);
+        return null;
+    }
+}
+
+export async function getActivedPlants() {
+    try {
+        const response = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/plant?Actived=true`);
+
+        if (response.data && response.data.isSuccess) {
+            console.log(response.data.result);
+            return response.data.result;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error('Error fetching data:', error);
+        return null;
+    }
+}
+
+export async function getPlantById(id) {
+    try {
+        const { data } = await axios.get(`${process.env.REACT_APP_API_ENDPOINT}/plant/${id}`);
+        if (data.isSuccess) return data.result;
+        else return null;
+    } catch (error) {
+        console.error('Có lỗi:', error);
+        return null;
+    }
+}
+
+export async function toggleActivedPlantById(id) {
+    try {
+        const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/plant/${id}`);
+        return data;
+    } catch (error) {
+        console.error('Có lỗi:', error);
+        return null;
+    }
+}
+
+export async function createPlant(plant) {
+    try {
+        const { data } = await axios.post(`${process.env.REACT_APP_API_ENDPOINT}/plant`, plant);
+
+        return data;
+    } catch (error) {
+        console.error('Có lỗi:', error);
+        return null;
+    }
+}
+
+export async function updatePlant(id, plant) {
+    try {
+        const { data } = await axios.put(`${process.env.REACT_APP_API_ENDPOINT}/plant/${id}`, plant);
+
+        return data;
+    } catch (error) {
+        console.error('Có lỗi:', error);
+        return null;
+    }
+}
+
+export async function deletePlantById(id) {
+    try {
+        const { data } = await axios.delete(`${process.env.REACT_APP_API_ENDPOINT}/plant/${id}`);
+        if (data.isSuccess) {
+            return data.result;
+        } else {
+            return null;
+        }
+    } catch (error) {
+        console.error('Có lỗi:', error);
         return null;
     }
 }
